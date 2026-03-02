561be7239fbd22707a2a76a5c5e62e51f5ab27a8
[Joy Swarnakar] <[joyswarnakar3@gmail.com]>
2026-03-03 05:25:18 +0530

    changes

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
-  {  // debasish 
+  {  // joy
     name:{
       type:String,
       require:[true,"Name is Required"],
--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/taskmanager")
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
